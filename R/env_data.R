a980aa9eb798e34e284432ba1dbbe52cd4b87d78
Wan-lang <74804383+Wan-lang@users.noreply.github.com>
2026-06-08 15:00:57 +0800

    Add files via upload

---
+++ R/env_data.R
@@ -0,0 +1,12 @@
+#' Environmental Data
+#'
+#' This dataset contains environmental variables that can be used to explain variations in microbiome data.
+#'
+#' @name env
+#' @format A data frame with [number of rows] rows and [number of columns] columns.
+#'   - Each row represents a sample.
+#'   - Each column represents an environmental variable.
+#' @source Example data created for demonstration purposes.
+#' @examples
+#' data(env)
+NULL
